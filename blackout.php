661b42d59e6e8ce513ba9a7901a8111dedabab8f
NullDev (Shadow) <shadow@nulldev.org>
2019-03-18 14:27:40 +0100

    Added blackout page template

---
+++ blackout.php
@@ -0,0 +1,184 @@
+
+<html>
+
+<head>
+    <title>#Blackout21</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="#Blackout21 #SaveYourInternet">
+    <meta name="theme-color" content="#000">
+    <meta itemprop="name" content="#Blackout21">
+    <meta itemprop="description" content="#Blackout21">
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content='#Blackout21' />
+    <meta name="twitter:description" content="#Blackout21" />
+    <link href="https://savetheinternet.info/blackout/blackout.css" rel="stylesheet">
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Raleway');
+
+        body {
+            background-color: #000;
+        }
+
+        div {
+            text-align: center;
+            position: absolute;
+            width: 300px;
+            top: 150px;
+            left: 50%;
+            margin: -50px 0 0 -150px;
+        }
+
+        h1 {
+            color: #FFF;
+            font-family: 'Raleway', sans-serif;
+            letter-spacing: 10px;
+            animation: text-flicker 30s linear infinite;
+        }
+
+        p {
+            color: #FFF;
+            font-family: 'Raleway', sans-serif;
+        }
+
+        .offset {
+            animation: letter-flicker 20s linear infinite;
+        }
+
+        @keyframes text-flicker {
+            0% {
+                opacity: 0.1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1);
+            }
+
+            2% {
+                opacity: 1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1);
+            }
+
+            8% {
+                opacity: 0.1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1);
+            }
+
+            9% {
+                opacity: 1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1);
+            }
+
+            12% {
+                opacity: 0.1;
+                text-shadow: 0px 0px rgba(255, 255, 255, 1);
+            }
+
+            20% {
+                opacity: 1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1)
+            }
+
+            25% {
+                opacity: 0.3;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1)
+            }
+
+            30% {
+                opacity: 1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1)
+            }
+
+            70% {
+                opacity: 0.7;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1)
+            }
+
+            72% {
+                opacity: 0.2;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1)
+            }
+
+            77% {
+                opacity: .9;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1)
+            }
+
+            100% {
+                opacity: .9;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1)
+            }
+        }
+
+        @keyframes letter-flicker {
+            0% {
+                opacity: 0.1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1);
+            }
+
+            2% {
+                opacity: 0.1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1);
+            }
+
+            4% {
+                opacity: 1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1);
+            }
+
+
+            19% {
+                opacity: 1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1);
+            }
+
+            21% {
+                opacity: 0.1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1);
+            }
+
+            23% {
+                opacity: 1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1);
+            }
+
+            80% {
+                opacity: 1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1);
+            }
+
+            83% {
+                opacity: 0.4;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1);
+            }
+
+            87% {
+                opacity: 1;
+                text-shadow: 0px 0px 29px rgba(255, 255, 255, 1);
+            }
+        }
+    </style>
+</head>
+
+<body class="bg-black text-white">
+
+    <img src="https://savetheinternet.info/blackout/blackout.gif" alt="blackout-banner" class="block mx-auto mt-12 md:mt-32">
+    <h1 class="text-center text-xl md:text-3xl mt-16">W<span class="offset">ha</span>t hap<span class="offset">pen</span>ed?</h1>
+    <p class="p-4 w-full md:p-0 md:w-2/3 mx-auto text-center mt-4">Lorem ipsum dolor sit amet consectetur adipisicing
+        elit.
+        Libero corrupti
+        odio corporis officia. Sed animi
+        adipisci quia sequi impedit voluptatibus iure, officia tempora. Eos quos impedit nisi voluptate molestias
+        nesciunt?Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio perspiciatis commodi eum, maiores eius
+        alias nisi eos debitis laborum. Fuga magni iure accusamus ab exercitationem in beatae facilis, est nobis? Lorem
+        ipsum dolor sit, amet consectetur adipisicing elit. Natus laborum similique id et, aspernatur dolore dolores
+        sit iusto praesentium cumque, culpa atque at. Impedit velit enim in beatae cumque reiciendis. Lorem ipsum,
+        dolor sit amet consectetur adipisicing elit. Provident repellendus ea sint velit fugiat aspernatur
+        reprehenderit asperiores, officiis vel quasi aut voluptatibus voluptatum, quam, laudantium odit qui sequi nobis
+        perferendis.</p>
+    <p class="text-center mt-8 font-bold">
+        <a class="text-xl mx-auto text-white no-underline italic" rel="noopener" href="https://savetheinternet.info/blackout">
+            More infos
+        </a></p>
+
+    <img src="https://savetheinternet.info/blackout/logo.png" alt="blackout-logo" class="block mx-auto mt-16 mb-16 md:mb-0 w-32">
+</body>
+
+</html>
